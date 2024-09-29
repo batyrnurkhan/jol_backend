c4893aa07e337aceea45506f06bcd6a7581a80f8
batyrnurkhan <makyzhanbatyr@gmail.com>
2024-09-29 19:40:36 +0500

    added city

--- trip_v2/admin.py
+++ trip_v2/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Route, Stop
+from .models import Route, Stop, City
 
 @admin.register(Route)
 class RouteAdmin(admin.ModelAdmin):
@@ -10,3 +10,5 @@ class RouteAdmin(admin.ModelAdmin):
 class StopAdmin(admin.ModelAdmin):
     list_display = ('route', 'name', 'travel_time_from_start', 'stop_time')
     search_fields = ('name',)
+
+admin.site.register(City)
