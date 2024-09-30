e72ccc4489dc49aa8361c32977d40079e010ec47
batyrnurkhan <makyzhanbatyr@gmail.com>
2024-09-30 14:21:06 +0500

    added id to trip admin

--- trip/admin.py
+++ trip/admin.py
@@ -3,6 +3,6 @@ from .models import Trip
 
 @admin.register(Trip)
 class TripAdmin(admin.ModelAdmin):
-    list_display = ('route', 'bus', 'driver', 'departure_time', 'start_date', 'end_date', 'ticket_price', 'frequency')
+    list_display = ('id', 'route', 'bus', 'driver', 'departure_time', 'start_date', 'end_date', 'ticket_price', 'frequency')
     list_filter = ('route', 'bus', 'driver', 'start_date', 'end_date', 'frequency')
     search_fields = ('route__start_city', 'route__end_city', 'bus__name', 'driver__full_name')
